61e5a6bf73c2c9e0bc1b6aa092ee2eece4d75bef
NicoleEB-UNF <nicklaus.e.b@gmail.com>
2023-07-14 00:59:43 -0400

    highlight

--- src/Home.hs
+++ src/Home.hs
@@ -28,6 +28,10 @@ getHomeR = defaultLayout $ do
                background-repeat: no-repeat;
                background-size: cover;
            }
+           
+           highlight {
+              background-color: rgba(210,228,242, 0.25);
+           }
 
            h1 {
                color: white; 
@@ -60,7 +64,7 @@ getHomeR = defaultLayout $ do
          <div class="bg-image"> 
          <h1> Bayside Medical Practice of Vilano Beach, LLC
               <p style="padding-top:30px;"> Primary practice headed by Tammy Dye.
-              <p> Contact us: tammydye@baysidemedical.net
+              <p> Contact us: <highlight>tammydye@baysidemedical.net
               <p> Coming soon to 101 Yacht Club Drive, St Augustine FL, 32084
               <i class="fa-solid fa-circle-nodes fa-spin" style="color: #d1edfe;"></i>
     |]
